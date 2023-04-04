d2a0eb7e3bec8b6236a33941d19bb4f063bb68f7
井澤伊織 <izawa.iori.tan@gmail.com>
2023-04-04 18:04:08 -0400

    Calculate productive for entire folder

--- P_VALUE_PM/main.py
+++ P_VALUE_PM/main.py
@@ -1,23 +1,31 @@
-import re
+import re, os
 
 def price(price_dic, goods):
     return sum([int(price_dic[good[0]])*int(good[1]) for good in goods])
 
 pat = re.compile(r'(.*?) = \{(\n|.)*?cost = (.*?)\n(\n|.)*?\}')
+pat_ftxt = re.compile(r"(pm_.*?) = {(\n|.)*?workforce_scaled = {((\n|.)*?)}(\n|.)*?level_scaled = {((\n|.)*?)}")
+pat_empl = re.compile(r'building_employment_(.*?)_add = (.*?)\n')
 pat_input = re.compile(r'building_input_(.*?)_add = (.*?)\n')
 pat_output = re.compile(r'building_output_(.*?)_add = (.*?)\n')
-pat_empl = re.compile(r'building_employment_(.*?)_add = (.*?)\n')
 
 with open("config.txt", "r", encoding='utf-8') as f:
     loc = f.readline().replace("\n", "")
 
+with open("config.txt", "r", encoding='utf-8') as f:
+    loc2 = f.readlines()[1].replace("\n", "")
 if __name__ == "__main__":
     with open(loc, "r", encoding='utf-8') as f:
         goods_prices = dict([(x[0], x[2]) for x in pat.findall(f.read())])
-    with open("production_method", "r", encoding='utf-8') as f:
-        pm_raw_text = f.read()
-        inp = pat_input.findall(pm_raw_text)
-        out = pat_output.findall(pm_raw_text)
-        employee_num = sum([int(pop_type[1]) for pop_type in pat_empl.findall(pm_raw_text)])
+        
+    g_u_dict = {}
+    for n in os.listdir(loc2):
+        with open(loc2 + "\\" + n, "r", encoding='utf-8') as f:
+            raw_txt = f.read()
+            for pm in pat_ftxt.findall(raw_txt):
+                o_i = price(goods_prices, pat_output.findall(pm[2])) - price(goods_prices, pat_input.findall(pm[2]))
+                e = sum([int(pop[1]) for pop in pat_empl.findall(pm[4])])
 
-    print("The p value is {}".format(52*(price(goods_prices, out) - price(goods_prices, inp))/employee_num))
+                g_u_dict[pm[0]] = (52*o_i)/e
+
+    print(g_u_dict)
